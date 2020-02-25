84f8c3571ebbef7d5a65dab7245d4771f863e7b6
Haruyoshi Toyama <duane0728@gmail.com>
2020-02-25 14:11:10 +0900

    Init

---
+++ src/main/java/com/htoyama/leetcode/001/Solution.kt
@@ -0,0 +1,17 @@
+package com.htoyama.leetcode.`001`
+
+class Solution {
+  /**
+   * https://leetcode.com/problems/two-sum
+   */
+  fun twoSum(nums: IntArray, target: Int): IntArray {
+    val map = HashMap<Int, Int>()
+    nums.forEachIndexed { index, value ->
+      if (map.contains(target - value)) {
+        return intArrayOf(map.getValue(target - value), index)
+      }
+      map[value] = index
+    }
+    return intArrayOf()
+  }
+}
