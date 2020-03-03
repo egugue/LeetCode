ebb13f1c14a62c384a79b5694d69f2278b761ce8
Haruyoshi Toyama <duane0728@gmail.com>
2020-03-03 23:33:30 +0900

    Solve 204

---
+++ src/main/java/com/htoyama/leetcode/204/Solution.kt
@@ -0,0 +1,35 @@
+package com.htoyama.leetcode.`204`
+
+fun main() {
+  Solution().apply {
+    println(countPrimes(10))
+    println(countPrimes(3))
+  }
+}
+
+class Solution {
+  fun countPrimes(n: Int): Int {
+    if (n <= 2) return 0
+
+    var sum = 1
+    for (i in 3 until n step 2) {
+      if (isPrime(i)) {
+        //println(i)
+        sum++
+      }
+    }
+
+    return sum
+  }
+
+  private fun isPrime(num: Int): Boolean {
+    if (num < 2) return false
+
+    val sqrt = Math.sqrt(num.toDouble()).toInt()
+    for (i in 2..sqrt) {
+      if (num % i == 0) return false
+    }
+
+    return true
+  }
+}
