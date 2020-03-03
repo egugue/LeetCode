905317ebc86474a160def60f0ae359c6354e28f5
Haruyoshi Toyama <duane0728@gmail.com>
2020-03-03 23:15:43 +0900

    Solve 217

---
+++ src/main/java/com/htoyama/leetcode/217/Solution.kt
@@ -0,0 +1,29 @@
+package com.htoyama.leetcode.`217`
+
+fun main() {
+  Solution().apply {
+    println(containsDuplicate(intArrayOf()))
+    println(containsDuplicate(intArrayOf(1,2,3,1)))
+    println(containsDuplicate(intArrayOf(1,2,3,4)))
+    println(containsDuplicate(intArrayOf(1,1,1,3,3,4,3,2,4,2)))
+  }
+}
+
+/**
+ * https://leetcode.com/problems/contains-duplicate/
+ */
+class Solution {
+  fun containsDuplicate(nums: IntArray): Boolean {
+    val set = HashSet<Int>()
+
+    for (num in nums) {
+      if (num in set) {
+        return true
+      }
+
+      set.add(num)
+    }
+
+    return false
+  }
+}
