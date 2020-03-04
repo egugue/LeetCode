0aeaccb984786ff19a7830c51bcc0c699ce182bf
Haruyoshi Toyama <duane0728@gmail.com>
2020-03-05 00:12:51 +0900

    Solve 185 more

--- sql/185.sql
+++ sql/185.sql
@@ -16,6 +16,9 @@ insert into Department (Id, Name) values ('2', 'Sales');
 
 
 # Write your MySQL query statement below
+
+# The below sql is original I submitted on 2020/3/5
+# The sql can be more readable because Subquery can be defined in where clause
 Select
     Department,
     Employee,
@@ -35,4 +38,40 @@ From (
 ) AS Temp
 Where _Rank <= 3
 Order By Department, Salary Desc
+;
+
+# The below sql can reduce nested sql compared to the previous sql.
+# But it takes more time (1530ms vs 785ms)
+Select
+    d1.Name AS Department,
+    e1.Name AS Employee,
+    e1.Salary As Salary
+From Department d1
+Inner Join Employee e1 On d1.Id = e1.DepartmentId
+Where 3 >= (
+      Select COUNT(Distinct e2.Salary)
+      From Employee e2
+      Where e1.DepartmentId = e2.DepartmentId
+            AND e1.Salary < e2.Salary
+    ) + 1
+Order By Department, Salary Desc
+;
+
+
+## This could work on MySQL8 or higher
+Select
+    Department,
+    Employee,
+    Salary
+From (
+    Select
+        d1.Name AS Department,
+        e1.Name AS Employee,
+        e1.Salary As Salary,
+        DENSE_RANK() OVER (PARTITION By d1.Id Order By e1.Salary Desc) As _Rank
+    From Department d1
+    Inner Join Employee e1 On d1.Id = e1.DepartmentId
+) AS Temp
+Where
+    _Rank <= 3
 ;
