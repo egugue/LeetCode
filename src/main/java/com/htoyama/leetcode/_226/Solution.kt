90d5d4bfd064506529ebc5e3d19257c3ab003823
Haruyoshi Toyama <duane0728@gmail.com>
2020-03-06 21:31:36 +0900

    Solve 226 using loop and queue

--- src/main/java/com/htoyama/leetcode/_226/Solution.kt
+++ src/main/java/com/htoyama/leetcode/_226/Solution.kt
@@ -1,11 +1,13 @@
 package com.htoyama.leetcode._226
 
 import com.htoyama.leetcode.utils.data.TreeNode
+import java.util.LinkedList
+import java.util.Queue
 
 fun main() {
   Solution().apply {
-    invertTree(TreeNode.of(4, 2, 7, 1, 3, 6, 9))
-    invertTree(TreeNode.of(4, 2, 7, null, 3, 6, null))
+    invertTree2(TreeNode.of(4, 2, 7, 1, 3, 6, 9))
+    invertTree2(TreeNode.of(4, 2, 7, null, 3, 6, null))
   }
 }
 
@@ -14,6 +16,11 @@ fun main() {
  */
 class Solution {
 
+  /**
+   * Case of using recursive call
+   *
+   * 148 ms	32.3 MB
+   */
   fun invertTree(root: TreeNode?): TreeNode? {
     if (root == null) return null
 
@@ -24,6 +31,36 @@ class Solution {
     root.left = root.right
     root.right = tmp
 
+    return root
+  }
+
+  /*
+           4
+         /   \
+        2     7
+       / \   / \
+      1   3 6   9
+   */
+  /**
+   * Case of using queue
+   *
+   * 144 ms	32.1 MB
+   */
+  fun invertTree2(root: TreeNode?): TreeNode? {
+    if (root == null) return null
+    val queue: Queue<TreeNode> = LinkedList()
+    queue.add(root)
+
+    while (!queue.isEmpty()) {
+      val current = queue.poll()!!
+
+      val temp = current.left
+      current.left = current.right
+      current.right = temp
+
+      current.left?.let { queue.add(it) }
+      current.right?.let { queue.add(it) }
+    }
     return root
   }
 }
