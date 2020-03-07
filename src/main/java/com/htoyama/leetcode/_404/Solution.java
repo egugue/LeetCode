38a0b10e77084b01a53f252b428b85437362ae14
Haruyoshi Toyama <duane0728@gmail.com>
2020-03-07 23:25:19 +0900

    Solve 404 using recursive call

--- src/main/java/com/htoyama/leetcode/_404/Solution.java
+++ src/main/java/com/htoyama/leetcode/_404/Solution.java
@@ -10,6 +10,9 @@ public class Solution {
 
   public static void main(String[] args) {
     Solution s = new Solution();
+    assertThat(s.sumOfLeftLeaves_lo(TreeNode.of(3, 9, 20, null, null, 15, 7))).isEqualTo(24);
+    assertThat(s.sumOfLeftLeaves_lo(TreeNode.of(1, 2, 3, 4, 5))).isEqualTo(4);
+
     assertThat(s.sumOfLeftLeaves(TreeNode.of(3, 9, 20, null, null, 15, 7))).isEqualTo(24);
     assertThat(s.sumOfLeftLeaves(TreeNode.of(1, 2, 3, 4, 5))).isEqualTo(4);
   }
@@ -17,10 +20,11 @@ public class Solution {
   /**
    * Case of using loop and queue
    *
-   * TODO: need to read the description carefully.
-   * the sum of all left "leaves" is mentioned here.
+   * 0 ms	37.6 MB	java
+   *
+   * TODO: need to read the description carefully. The sum of all left "leaves" is mentioned here.
    */
-  public int sumOfLeftLeaves(TreeNode root) {
+  public int sumOfLeftLeaves_lo(TreeNode root) {
     if (root == null) return 0;
 
     ArrayDeque<TreeNode> queue = new ArrayDeque<>();
@@ -40,6 +44,29 @@ public class Solution {
       if(curr.right != null) {
         queue.add(curr.right);
       }
+    }
+
+    return sum;
+  }
+
+  /**
+   * Case of using recursive
+   *
+   * 0 ms	37.5 MB	java
+   */
+  public int sumOfLeftLeaves(TreeNode root) {
+    if (root == null) return 0;
+
+    int sum = 0;
+    if (root.left != null) {
+      if (root.left.left == null && root.left.right == null) {
+        sum += root.left.val;
+      } else {
+        sum += sumOfLeftLeaves(root.left);
+      }
+    }
+    if (root.right != null) {
+      sum += sumOfLeftLeaves(root.right);
     }
 
     return sum;
