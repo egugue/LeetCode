426ba513ac6990aba098e1a4e7dea4cdb4fe2989
Haruyoshi Toyama <duane0728@gmail.com>
2020-03-08 00:10:31 +0900

    Solve 405

---
+++ src/main/java/com/htoyama/leetcode/_405/Solution.java
@@ -0,0 +1,32 @@
+package com.htoyama.leetcode._405;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+
+    assertThat(s.toHex(0)).isEqualTo("0");
+    assertThat(s.toHex(18)).isEqualTo("12");
+    assertThat(s.toHex(-18)).isEqualTo("ffffffee");
+    assertThat(s.toHex(-1)).isEqualTo("ffffffff");
+  }
+
+  private static char[] table = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+  /**
+   * TODO: Must review later
+   */
+  public String toHex(int num) {
+    if (num == 0) return "0";
+
+    StringBuilder sb = new StringBuilder();
+    while (num != 0) {
+      sb.append(table[num & 15]);
+      num = (num >>> 4);
+    }
+
+    return sb.reverse().toString();
+  }
+}
