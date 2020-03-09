7019dd97e594feaf064e441f2f8d4f797e109894
Haruyoshi Toyama <duane0728@gmail.com>
2020-03-09 20:23:56 +0900

    Solve 387. First Unique Character in a String

---
+++ src/main/java/com/htoyama/leetcode/_387/Solution.java
@@ -0,0 +1,28 @@
+package com.htoyama.leetcode._387;
+
+/**
+ * https://leetcode.com/problems/first-unique-character-in-a-string/
+ */
+class Solution {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.firstUniqChar("leetcode"));
+    System.out.println(s.firstUniqChar("loveleetcode"));
+  }
+
+  /**
+   * 7 ms	41.3 MB
+   */
+  public int firstUniqChar(String s) {
+    int[] table = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+      table[s.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < s.length(); i++) {
+      if (table[s.charAt(i) - 'a'] == 1) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
