843895cc76491b839797bf94e2bbf03925913bd6
Haruyoshi Toyama <duane0728@gmail.com>
2020-03-20 00:36:12 +0900

    Solve 75. Sort Colors with a one-shot algo

--- src/main/java/com/htoyama/leetcode/_075/Solution.java
+++ src/main/java/com/htoyama/leetcode/_075/Solution.java
@@ -4,6 +4,8 @@ import com.htoyama.leetcode.utils.Array_;
 import com.htoyama.leetcode.utils.Level;
 import com.htoyama.leetcode.utils.Sort;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -20,21 +22,29 @@ class Solution {
 
     ints = new int[]{2, 0, 2, 1, 1, 0};
     s.sortColors(ints);
+    System.out.println(Arrays.toString(ints));
     assertThat(ints).containsExactly(0, 0, 1, 1, 2, 2);
 
     ints = new int[]{2, 0};
     s.sortColors(ints);
+    System.out.println(Arrays.toString(ints));
     assertThat(ints).containsExactly(0, 2);
 
     ints = new int[]{0, 0};
     s.sortColors(ints);
+    System.out.println(Arrays.toString(ints));
     assertThat(ints).containsExactly(0, 0);
+
+    ints = new int[]{2, 0, 1};
+    s.sortColors(ints);
+    System.out.println(Arrays.toString(ints));
+    assertThat(ints).containsExactly(0, 1, 2);
   }
 
   /**
    * 0 ms	38.2 MB
    */
-  public void sortColors(int[] nums) {
+  public void sortColors_(int[] nums) {
     if (nums.length <= 1) return;
 
     int firstNonZeroIndex = 0;
@@ -53,6 +63,33 @@ class Solution {
     }
     for (int i = firstTwoIndex; i < nums.length; i++) {
       nums[i] = 2;
+    }
+  }
+
+  /**
+   * 0 ms	38.3 MB
+   *
+   * TODO: Must try later with this one-shot algorithm
+   */
+  public void sortColors(int[] nums) {
+    if (nums.length <= 1) return;
+
+    int firstNonZeroIndex = 0;
+    int lastNonTwoIndex = nums.length - 1;
+    int i = 0;
+    while (i <= lastNonTwoIndex) {
+      switch (nums[i]) {
+        case 0:
+          nums[i++] = nums[firstNonZeroIndex];
+          nums[firstNonZeroIndex++] = 0;
+          break;
+        case 2:
+          nums[i] = nums[lastNonTwoIndex];
+          nums[lastNonTwoIndex--] = 2;
+          break;
+        default:
+          i++;
+      }
     }
   }
 }
