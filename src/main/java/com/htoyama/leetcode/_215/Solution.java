943d3df8c6bcda0328df91c644661aac01952ea6
Haruyoshi Toyama <duane0728@gmail.com>
2020-03-23 22:24:32 +0900

    Solve 215. Kth Largest Element in an Array

---
+++ src/main/java/com/htoyama/leetcode/_215/Solution.java
@@ -0,0 +1,30 @@
+package com.htoyama.leetcode._215;
+
+import com.htoyama.leetcode.utils.Array_;
+import com.htoyama.leetcode.utils.Level;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 215. Kth Largest Element in an Array
+ */
+@Level.Medium
+@Array_
+class Solution {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    assertThat(s.findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2)).isEqualTo(5);
+    assertThat(s.findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4)).isEqualTo(4);
+    assertThat(s.findKthLargest(new int[]{3, 3, 3, 3, 1, 2}, 4)).isEqualTo(3);
+  }
+
+  /**
+   * 2 ms	41.3 MB
+   */
+  public int findKthLargest(int[] nums, int k) {
+    Arrays.sort(nums);
+    return nums[nums.length - k];
+  }
+}
