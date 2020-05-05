22125000f47fa807e4e0f7d8e10dfba2e75e3000
Haruyoshi Toyama <duane0728@gmail.com>
2020-05-05 20:12:25 +0900

    Improve runtime

--- jvm/src/main/java/com/htoyama/leetcode/_011/Solution.java
+++ jvm/src/main/java/com/htoyama/leetcode/_011/Solution.java
@@ -14,29 +14,32 @@ class Solution {
 
   public static void main(String[] args) {
     Solution s = new Solution();
-    assertThat(s.maxArea(new int[]{1,8,6,2,5,4,8,3,7})).isEqualTo(49);
+    assertThat(s.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7})).isEqualTo(49);
   }
 
   /**
-   * 2 ms	39.7 MB
+   * 1 ms	40 MB
    */
   public int maxArea(int[] height) {
-    if(height.length <= 1) return 0;
+    if (height.length <= 1) return 0;
 
     int left = 0;
     int right = height.length - 1;
-    int max = 0;
+    int maxArea = 0;
     while (left != right) {
-      int area = (right - left) *  Math.min(height[left], height[right]);
-      max = Math.max(max, area);
+      int minHeight = Math.min(height[left], height[right]);
+      int area = (right - left) * minHeight;
+      maxArea = Math.max(maxArea, area);
 
       if (height[left] < height[right]) {
-        left++;
+        int h = height[left];
+        while(++left != right && h > height[left]);
       } else {
-        right--;
+        int h = height[right];
+        while(--right != left && h > height[right]);
       }
     }
 
-    return max;
+    return maxArea;
   }
 }
