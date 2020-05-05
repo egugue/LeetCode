d5125de09e69444b80947b3e4054d611276ff352
Haruyoshi Toyama <duane0728@gmail.com>
2020-05-05 20:35:01 +0900

    Improve more

--- jvm/src/main/java/com/htoyama/leetcode/_011/Solution.java
+++ jvm/src/main/java/com/htoyama/leetcode/_011/Solution.java
@@ -14,11 +14,12 @@ class Solution {
 
   public static void main(String[] args) {
     Solution s = new Solution();
-    assertThat(s.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7})).isEqualTo(49);
+    //assertThat(s.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7})).isEqualTo(49);
+    assertThat(s.maxArea(new int[]{5, 8, 6, 2, 5, 4, 8, 1, 5})).isEqualTo(40);
   }
 
   /**
-   * 1 ms	40 MB
+   * 1 ms	39.6 MB
    */
   public int maxArea(int[] height) {
     if (height.length <= 1) return 0;
@@ -31,13 +32,18 @@ class Solution {
       int area = (right - left) * minHeight;
       maxArea = Math.max(maxArea, area);
 
-      if (height[left] < height[right]) {
-        int h = height[left];
-        while(++left != right && h > height[left]);
-      } else {
-        int h = height[right];
-        while(--right != left && h > height[right]);
-      }
+      while(height[left] <= minHeight && left != right) left++;
+      while(height[right] <= minHeight && left != right) right--;
+
+      // the code below takes more time than above if height[left] == height[right]
+      // because in such case, left also needs to increment but left will increment in the next loop
+//      if (height[left] < height[right]) {
+//        int h = height[left];
+//        while(++left != right && h > height[left]);
+//      } else {
+//        int h = height[right];
+//        while(--right != left && h > height[right]);
+//      }
     }
 
     return maxArea;
