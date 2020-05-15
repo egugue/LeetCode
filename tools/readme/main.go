3f56213aa87a835f243289d0b35d8fd9067674e9
Haruyoshi Toyama <duane0728@gmail.com>
2020-05-15 23:36:21 +0900

    Tweak

--- tools/readme/main.go
+++ tools/readme/main.go
@@ -10,6 +10,23 @@ import (
 )
 
 func main() {
+	setUpLogger()
+
+	response, err := leetcode.GetProblemResponse()
+	if err != nil {
+		zap.S().Errorf("couldn't retrieve problems\n%v\n", err)
+		os.Exit(1)
+	}
+
+	solutionsTable := solution.GetAllSolutions()
+
+	err = markdown.WriteREADME(response, &solutionsTable)
+	if err != nil {
+		zap.S().Errorf("couldn't write README\n%v\n", err)
+	}
+}
+
+func setUpLogger() {
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		log.Fatalf("Initialization error.\n%v\n", err)
@@ -18,16 +35,4 @@ func main() {
 	undo := zap.ReplaceGlobals(logger)
 	defer undo()
 
-	response, err := leetcode.GetProblemResponse()
-	if err != nil {
-		zap.S().Error("couldn't retrieve problems\n%v\n", err)
-		os.Exit(1)
-	}
-
-	solutionsTable := solution.GetAllSolutions()
-
-	err = markdown.WriteREADME(response, &solutionsTable)
-	if err != nil {
-		zap.S().Error("couldn't write README\n%v\n", err)
-	}
 }
