8bb2add7b2c996150a4c1607b5728087fab7d696
Haruyoshi Toyama <duane0728@gmail.com>
2020-05-23 00:05:51 +0900

    Remove visited table

--- jvm/src/main/java/com/htoyama/leetcode/_048/Solution.java
+++ jvm/src/main/java/com/htoyama/leetcode/_048/Solution.java
@@ -11,12 +11,13 @@ class Solution {
       {4, 5, 6},
       {7, 8, 9}
     };
-    matrix = new int[][]{
-      {1, 2, 3, 4},
-      {5, 6, 7, 8},
-      {9, 10, 11, 12},
-      {13, 14, 15, 16},
-    };
+//    matrix = new int[][]{
+//      {1, 2, 3, 4},
+//      {5, 6, 7, 8},
+//      {9, 10, 11, 12},
+//      {13, 14, 15, 16},
+//    };
+
     s.rotate(matrix);
 
     for (int[] ints : matrix) {
@@ -29,25 +30,20 @@ class Solution {
    */
   public void rotate(int[][] matrix) {
     if (matrix.length == 0 || matrix[0].length != matrix.length) return;
-    boolean[][] visited = new boolean[matrix.length][matrix[0].length];
 
     for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[0].length; j++) {
-        int from1 = i;
-        int from2 = j;
-        int prev = matrix[from1][from2];
+      for (int j = 0; j < i; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = tmp;
+      }
+    }
 
-        while (!visited[from1][from2]) {
-          int to1 = from2;
-          int to2 = Math.abs(from1 - matrix.length + 1);
-          int tmp = matrix[to1][to2];
-          matrix[to1][to2] = prev;
-          prev = tmp;
-          visited[from1][from2] = true;
-          from1 = to1;
-          from2 = to2;
-        }
-
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix.length / 2; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[i][matrix.length - 1 - j];
+        matrix[i][matrix.length - 1 - j] = tmp;
       }
     }
   }
