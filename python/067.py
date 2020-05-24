0fcbbf613348ab02bc58215d1fb0c713cd36cfca
Haruyoshi Toyama <duane0728@gmail.com>
2020-05-25 01:43:15 +0900

    Improve code

--- python/067.py
+++ python/067.py
@@ -1,34 +1,18 @@
-from collections import deque
-
-
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        # 24 ms	13.9 MB
+        # 32 ms	13.9 MB
         ai, bi = len(a) - 1, len(b) - 1
-        binaries, carry = deque(), 0
-        while ai >= 0 and bi >= 0:
-            inter = int(a[ai]) + int(b[bi]) + carry
+        binaries, carry = "", 0
+        while ai >= 0 or bi >= 0 or carry:
+            inter = (int(a[ai]) if ai >= 0 else 0) \
+                    + (int(b[bi]) if bi >= 0 else 0) \
+                    + carry
             carry = inter // 2
-            binaries.appendleft(str(inter % 2))
+            binaries = str(inter % 2) + binaries
             ai -= 1
             bi -= 1
 
-        while ai >= 0:
-            inter = int(a[ai]) + carry
-            carry = inter // 2
-            binaries.appendleft(str(inter % 2))
-            ai -= 1
-
-        while bi >= 0:
-            inter = int(b[bi]) + carry
-            carry = inter // 2
-            binaries.appendleft(str(inter % 2))
-            bi -= 1
-
-        if carry:
-            binaries.appendleft(str(carry))
-
-        return "".join(binaries)
+        return binaries
 
 
 if __name__ == '__main__':
