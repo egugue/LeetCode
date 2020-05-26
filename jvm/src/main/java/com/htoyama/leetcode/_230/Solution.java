57b11ca3daaa015a11200b6eca8a37e62b18e6f1
Haruyoshi Toyama <duane0728@gmail.com>
2020-05-27 00:57:48 +0900

    Solve 230. Kth Smallest Element in a BST

---
+++ jvm/src/main/java/com/htoyama/leetcode/_230/Solution.java
@@ -0,0 +1,46 @@
+package com.htoyama.leetcode._230;
+
+import com.htoyama.leetcode.utils.data.TreeNode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Solution {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    assertThat(s.kthSmallest(TreeNode.of(3, 1, 4, null, 2), 1)).isEqualTo(1);
+    assertThat(s.kthSmallest(TreeNode.of(5, 3, 7, 2, 4, 6, null, 1), 3)).isEqualTo(3);
+    assertThat(s.kthSmallest(TreeNode.of(
+      31, 30, 48, 3, null, 38, 49, 0, 16, 35, 47, null, null, null, 2, 15, 27, 33, 37, 39, null, 1, null, 5, null, 22, 28, 32, 34, 36, null, null, 43, null, null, 4, 11, 19, 23, null, 29, null, null, null, null, null, null, 40, 46, null, null, 7, 14, 17, 21, null, 26, null, null, null, 41, 44, null, 6, 10, 13, null, null, 18, 20, null, 25, null, null, 42, null, 45, null, null, 8, null, 12, null, null, null, null, null, 24, null, null, null, null, null, null, 9
+    ), 1)).isEqualTo(0);
+  }
+
+  /**
+   * 0 ms	39.1 MB
+   */
+  public int kthSmallest(TreeNode root, int k) {
+    Result result = inOrder(root, k, new Result());
+    return result.kthSmallest;
+  }
+
+  private Result inOrder(TreeNode root, int k, Result result) {
+    if (root == null) return result;
+
+    result = inOrder(root.left, k, result);
+    if (result.found) return result;
+
+    if (++result.visitedCount == k) {
+      result.found = true;
+      result.kthSmallest = root.val;
+      return result;
+    }
+
+    result = inOrder(root.right, k, result);
+    return result;
+  }
+
+  static class Result{
+    boolean found = false;
+    int kthSmallest;
+    int visitedCount;
+  }
+}
