f9852e61ef2f5395f99e634be7623748f9983431
Haruyoshi Toyama <duane0728@gmail.com>
2020-06-01 20:54:19 +0900

    Solve 560. Subarray Sum Equals K

--- jvm/src/main/java/com/htoyama/leetcode/_560/Solution.java
+++ jvm/src/main/java/com/htoyama/leetcode/_560/Solution.java
@@ -1,17 +1,25 @@
 package com.htoyama.leetcode._560;
 
+import java.util.HashMap;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Solution {
   public static void main(String[] args) {
     Solution s = new Solution();
-    assertThat(s.subarraySum(new int[]{1, 1, 1}, 2)).isEqualTo(2);
+//    assertThat(s.subarraySum(new int[]{1, 1, 1}, 2)).isEqualTo(2);
+    assertThat(s.subarraySum(new int[]{3, 4, -4, 0, 3}, 3)).isEqualTo(4);
+  }
+
+  public int subarraySum(int[] nums, int k) {
+//   return cumulativeSum(nums, k);
+    return hashMap(nums, k);
   }
 
   /**
    * 549 ms	41 MB
    */
-  public int subarraySum(int[] nums, int k) {
+  private static int cumulativeSum(int [] nums, int k) {
     if (nums.length == 0) return 0;
 
     for (int i = 1; i < nums.length; i++) {
@@ -25,6 +33,25 @@ class Solution {
         if (nums[end] - sv == k)
           count++;
       }
+    }
+
+    return count;
+  }
+
+  /**
+   * 13 ms	40.7 MB
+   */
+  private static int hashMap(int[] nums, int k) {
+    if (nums.length == 0) return 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+    map.put(0, 1);
+
+    int sum = 0;
+    int count = 0;
+    for (int num : nums) {
+      sum += num;
+      count += map.getOrDefault(sum - k, 0);
+      map.put(sum, map.getOrDefault(sum, 0) + 1);
     }
 
     return count;
