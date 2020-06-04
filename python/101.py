b0eb1ef2041a33934c648e66a968c9ba1e028ca9
Haruyoshi Toyama <duane0728@gmail.com>
2020-06-04 20:30:52 +0900

    Solve 101. Symmetric Tree

--- python/101.py
+++ python/101.py
@@ -3,8 +3,37 @@ from shared.TreeNode import TreeNode
 
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        return iterative(root)
+        return iterative2(root)
+        # return iterative(root)
         # return recursive(root, root)
+
+
+def iterative2(root: TreeNode) -> bool:
+    # 36 ms	13.9 MB
+    if root is None:
+        return True
+    nodes = [root]
+
+    while len(nodes) != 0:
+        length = len(nodes)
+        for i in range(length // 2):
+            node1 = nodes[i]
+            node2 = nodes[-i - 1]
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2:
+                return False
+            if node1.val != node2.val:
+                return False
+
+        for i in range(length):
+            node = nodes[0]
+            del nodes[0]
+            if node:
+                nodes.append(node.left)
+                nodes.append(node.right)
+
+    return True
 
 
 def iterative(root: TreeNode) -> bool:
@@ -38,3 +67,9 @@ def recursive(left: TreeNode, right: TreeNode) -> bool:
     if left.val != right.val:
         return False
     return recursive(left.left, right.right) and recursive(left.right, right.left)
+
+
+if __name__ == '__main__':
+    a = [1, 2, 3]
+    print(a[0])
+    print(a[-0])
