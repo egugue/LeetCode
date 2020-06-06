87756a70d5c6d1f1d27e326de4e9efe7ec54cf05
Haruyoshi Toyama <duane0728@gmail.com>
2020-06-06 11:38:41 +0900

    Solve 108. Convert Sorted Array to Binary Search Tree

--- python/108.py
+++ python/108.py
@@ -5,7 +5,30 @@ from shared.TreeNode import TreeNode
 
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        return recursive(nums)
+        return iterative(nums)
+        # return recursive(nums)
+
+
+def iterative(nums: List[int]) -> TreeNode:
+    # 68 ms	15.8 MB
+    if len(nums) == 0:
+        return None
+
+    root = TreeNode(nums[len(nums) // 2])
+    stack = [(root, nums)]
+    while len(stack) != 0:
+        node, children = stack.pop()
+        mid = len(children) // 2
+        left = children[:mid]
+        right = children[mid + 1:]
+        if len(left) != 0:
+            node.left = TreeNode(left[len(left) // 2])
+            stack.append((node.left, left))
+        if len(right) != 0:
+            node.right = TreeNode(right[len(right) // 2])
+            stack.append((node.right, right))
+
+    return root
 
 
 def recursive(nums: List[int]) -> TreeNode:
