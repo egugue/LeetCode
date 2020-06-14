fd938d1a13259bf3c6d02df127df94016776bbc3
Haruyoshi Toyama <duane0728@gmail.com>
2020-06-14 15:42:37 +0900

    Solve 198. House Robber

---
+++ python/198.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # 32 ms	13.8 MB
+        length = len(nums)
+        if length <= 1:
+            return 0 if length == 0 else nums[0]
+
+        dp = [0] * length
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
+        for i in range(2, length):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+
+        return dp[length - 1]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.rob([1, 2, 3, 1]))
+    print(s.rob([2, 7, 9, 3, 1]))
