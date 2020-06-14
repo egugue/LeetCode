ece6210b2b9a880364133676a5b9e8fe17e3c4df
Haruyoshi Toyama <duane0728@gmail.com>
2020-06-14 17:42:21 +0900

    Solve using hashmap

--- jvm/src/main/java/com/htoyama/leetcode/_204/Solution.kt
+++ jvm/src/main/java/com/htoyama/leetcode/_204/Solution.kt
@@ -13,17 +13,37 @@ fun main() {
 class Solution {
   // TODO: Have to Review later and implement more efficient
   fun countPrimes(n: Int): Int {
-    if (n <= 2) return 0
+    return hashmap(n)
+    // if (n <= 2) return 0
+    //
+    // var sum = 1
+    // for (i in 3 until n step 2) {
+    //   if (isPrime(i)) {
+    //     //println(i)
+    //     sum++
+    //   }
+    // }
+    //
+    // return sum
+  }
 
-    var sum = 1
-    for (i in 3 until n step 2) {
-      if (isPrime(i)) {
-        //println(i)
-        sum++
+  /**
+   * 212 ms	36.7 MB
+   */
+  private fun hashmap(nums: Int): Int{
+    if (nums <= 2) return 0;
+
+    val isNotPrimes = BooleanArray(nums)
+    var count = 0
+    for (i in 2 until nums) {
+      if (isNotPrimes[i]) continue
+
+      count++
+      for (j in i * 2 until nums step i) {
+        isNotPrimes[j] = true
       }
     }
-
-    return sum
+    return count
   }
 
   private fun isPrime(num: Int): Boolean {
