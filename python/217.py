8647b46ea12c7a87aa58eb156b42e17094bb729a
Haruyoshi Toyama <duane0728@gmail.com>
2020-06-16 23:20:34 +0900

    Solve another solution

--- python/217.py
+++ python/217.py
@@ -4,6 +4,7 @@ from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return sets(nums)
+        # return sort(nums)
 
 
 def sets(nums: List[int]) -> bool:
@@ -16,4 +17,19 @@ def sets(nums: List[int]) -> bool:
         if num in unique:
             return True
         unique.add(num)
+    return False
+
+
+def sort(nums: List[int]) -> bool:
+    # 124 ms	19.1 MB
+    length = len(nums)
+    if length <= 1:
+        return False
+
+    nums = sorted(nums)
+    prev = nums[0]
+    for i in range(1, length):
+        if prev == nums[i]:
+            return True
+        prev = nums[i]
     return False
