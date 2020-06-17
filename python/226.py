583b6b6db5f620af7515ebab52617f0cf1a0dab5
Haruyoshi Toyama <duane0728@gmail.com>
2020-06-17 22:28:59 +0900

    Solve with another solution

--- python/226.py
+++ python/226.py
@@ -3,7 +3,30 @@ from shared.TreeNode import TreeNode
 
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
-        return recursive(root)
+        return iterative(root)
+        # return recursive(root)
+
+
+def iterative(root: TreeNode) -> TreeNode:
+    # 32 ms	13.7 MB
+    if not root:
+        return root
+
+    from collections import deque
+    queue = deque()
+    queue.append(root)
+    while len(queue) != 0:
+        node = queue.pop()
+        tmp = node.left
+        node.left = node.right
+        node.right = tmp
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+    return root
 
 
 def recursive(root: TreeNode) -> TreeNode:
