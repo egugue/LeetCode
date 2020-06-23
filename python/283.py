67e29e380ba503de8ad4e5f91f8c804d84fb2fce
Haruyoshi Toyama <duane0728@gmail.com>
2020-06-23 22:59:25 +0900

    Solve with another solution

--- python/283.py
+++ python/283.py
@@ -3,25 +3,46 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        # 40 ms	15 MB
-        length = len(nums)
-        if length <= 1:
-            return
+        return solution2(nums)
+        # return solution1(nums)
 
-        first_zero = 0
-        while first_zero < length and nums[first_zero] != 0:
-            first_zero += 1
 
-        zero_index = first_zero
-        for i in range(first_zero + 1, length):
-            if nums[i] != 0:
-                nums[zero_index] = nums[i]
-                zero_index += 1
+def solution2(nums: List[int]):
+    # 48 ms	15.1 MB
+    length = len(nums)
+    if length <= 1:
+        return
 
-        for i in range(zero_index, length):
-            nums[i] = 0
+    insert_position = 0
+    for _, num in enumerate(nums):
+        if num != 0:
+            nums[insert_position] = num
+            insert_position += 1
 
-        # print(nums)
+    for i in range(insert_position, length):
+        nums[i] = 0
+
+
+def solution1(nums: List[int]):
+    # 40 ms	15 MB
+    length = len(nums)
+    if length <= 1:
+        return
+
+    first_zero = 0
+    while first_zero < length and nums[first_zero] != 0:
+        first_zero += 1
+
+    zero_index = first_zero
+    for i in range(first_zero + 1, length):
+        if nums[i] != 0:
+            nums[zero_index] = nums[i]
+            zero_index += 1
+
+    for i in range(zero_index, length):
+        nums[i] = 0
+
+    # print(nums)
 
 
 if __name__ == '__main__':
