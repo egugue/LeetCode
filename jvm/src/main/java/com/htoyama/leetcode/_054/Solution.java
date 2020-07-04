b6d1963a1db30d1265abab99543b29e0a6823bdf
Haruyoshi Toyama <duane0728@gmail.com>
2020-07-04 22:16:37 +0900

    Solve with another solution

--- jvm/src/main/java/com/htoyama/leetcode/_054/Solution.java
+++ jvm/src/main/java/com/htoyama/leetcode/_054/Solution.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.contentOf;
 
 class Solution {
   public static void main(String[] args) {
@@ -26,20 +25,62 @@ class Solution {
     assertThat(integers).containsExactly(1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7);
   }
 
-  /**
-   * 0 ms	37.8 MB
-   */
   public List<Integer> spiralOrder(int[][] matrix) {
     if (matrix.length == 0 || matrix[0].length == 0) return Collections.emptyList();
     ArrayList<Integer> order = new ArrayList<>(matrix.length * matrix[0].length);
-    traverse(
-      matrix, 1, order,
-      0, matrix.length - 1, 0, matrix[0].length - 1
-    );
+//    recursive(
+//      matrix, 1, order,
+//      0, matrix.length - 1, 0, matrix[0].length - 1
+//    );
+    iterative(matrix, order);
     return order;
   }
 
-  private static void traverse(
+  /**
+   * 0 ms	38.9 MB
+   */
+  private static void iterative(
+    int[][] matrix, ArrayList<Integer> order
+  ) {
+    int type = 1;
+    int top = 0, bottom = matrix.length - 1;
+    int left = 0, right = matrix[0].length - 1;
+
+    while (bottom - top + 1 != 0 && right - left + 1 != 0) {
+      switch (type) {
+        // left to right
+        case 1:
+          for (int j = left; j <= right; j++) order.add(matrix[top][j]);
+          type = 2;
+          top += 1;
+          break;
+        // top to bottom
+        case 2:
+          for (int i = top; i <= bottom; i++) order.add(matrix[i][right]);
+          type = 3;
+          right -= 1;
+          break;
+
+        // right to left
+        case 3:
+          for (int j = right; j >= left; j--) order.add(matrix[bottom][j]);
+          type = 4;
+          bottom -= 1;
+          break;
+
+        // bottom to top
+        case 4:
+          for (int i = bottom; i >= top; i--) order.add(matrix[i][left]);
+          type = 1;
+          left += 1;
+      }
+    }
+  }
+
+  /**
+   * 0 ms	37.8 MB
+   */
+  private static void recursive(
     int[][] matrix, int type, ArrayList<Integer> order,
     int top, int bottom, int left, int right
   ) {
@@ -49,7 +90,7 @@ class Solution {
       // left to right
       case 1:
         for (int j = left; j <= right; j++) order.add(matrix[top][j]);
-        traverse(
+        recursive(
           matrix, 2, order,
           top + 1, bottom, left, right
         );
@@ -58,7 +99,7 @@ class Solution {
       // top to bottom
       case 2:
         for (int i = top; i <= bottom; i++) order.add(matrix[i][right]);
-        traverse(
+        recursive(
           matrix, 3, order,
           top, bottom, left, right - 1
         );
@@ -67,7 +108,7 @@ class Solution {
       // right to left
       case 3:
         for (int j = right; j >= left; j--) order.add(matrix[bottom][j]);
-        traverse(
+        recursive(
           matrix, 4, order,
           top, bottom - 1, left, right
         );
@@ -76,7 +117,7 @@ class Solution {
       // bottom to top
       case 4:
         for (int i = bottom; i >= top; i--) order.add(matrix[i][left]);
-        traverse(
+        recursive(
           matrix, 1, order,
           top, bottom, left + 1, right
         );
