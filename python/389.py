4542a8485c2c339b188e52029b0a9d2284d131ec
Haruyoshi Toyama <duane0728@gmail.com>
2020-07-06 23:59:05 +0900

    Solve with another solution

--- python/389.py
+++ python/389.py
@@ -1,28 +1,43 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        # 56 ms	13.9 MB
-        length = len(s)
-        if length == 0:
-            return t
-        table = {}
-        for i in range(length):
-            ch = s[i]
-            if ch in table:
-                table[ch] += 1
-            else:
-                table[ch] = 1
+        return diff(s, t)
+        # return using_dic(s, t)
 
-        t_length = len(t)
-        for i in range(t_length):
-            ch = t[i]
-            if ch in table:
-                if table[ch] == 0:
-                    return ch
-                table[ch] -= 1
-            else:
+
+def diff(s: str, t: str) -> str:
+    # 44 ms	13.8 MB
+    ch = ord(t[-1])
+    length = len(s)
+    for i in range(length):
+        ch -= ord(s[i])
+        ch += ord(t[i])
+    return chr(ch)
+
+
+def using_dic(s: str, t: str) -> str:
+    # 56 ms	13.9 MB
+    length = len(s)
+    if length == 0:
+        return t
+    table = {}
+    for i in range(length):
+        ch = s[i]
+        if ch in table:
+            table[ch] += 1
+        else:
+            table[ch] = 1
+
+    t_length = len(t)
+    for i in range(t_length):
+        ch = t[i]
+        if ch in table:
+            if table[ch] == 0:
                 return ch
+            table[ch] -= 1
+        else:
+            return ch
 
-        return ""
+    return ""
 
 
 if __name__ == '__main__':
@@ -31,4 +46,6 @@ if __name__ == '__main__':
     print(s.findTheDifference("abca", "abcaa"))
     print(s.findTheDifference("abcd", "abcde"))
     print(s.findTheDifference("abcd", "abcdd"))
-
+    a = "ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor"
+    b = "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj"
+    print(s.findTheDifference(a, b))
