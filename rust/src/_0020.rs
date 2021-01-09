a75c32f295f4cee00104366b477962357c87c53b
Haruyoshi Toyama <duane0728@gmail.com>
2021-01-10 00:19:56 +0900

    Solve with another solution

--- rust/src/_0020.rs
+++ rust/src/_0020.rs
@@ -1,7 +1,8 @@
 struct Solution;
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        Solution::stack(s)
+        // Solution::stack(s)
+        Solution::stack_map(s)
     }
 
     /// 0 ms	1.9 MB
@@ -22,6 +23,33 @@ impl Solution {
                 }
                 ']' => {
                     if stack.pop().unwrap_or('_') != '[' {
+                        return false;
+                    }
+                }
+                _ => {
+                    return false;
+                }
+            }
+        }
+
+        stack.is_empty()
+    }
+
+    /// 0 ms	2 MB
+    fn stack_map(s: String) -> bool {
+        use std::collections::HashMap;
+        let mut stack = Vec::new();
+        let close_open_table = [(')', '('), ('}', '{'), (']', '[')]
+            .iter()
+            .cloned()
+            .collect::<HashMap<char, char>>();
+
+        for char in s.chars() {
+            match char {
+                '(' | '{' | '[' => stack.push(char),
+                ')' | '}' | ']' => {
+                    let open = close_open_table.get(&char).unwrap();
+                    if open != &stack.pop().unwrap_or('_') {
                         return false;
                     }
                 }
@@ -54,5 +82,21 @@ mod tests {
     )]
     fn stack(s: &str, expected: bool) {
         assert_eq!(Solution::stack(s.to_string()), expected)
+    }
+
+    #[rstest(
+        s,
+        expected,
+        case("()", true),
+        case("()[]{}", true),
+        case("(]", false),
+        case("([)]", false),
+        case("{[]}", true),
+        case("{", false),
+        case("}", false),
+        ::trace
+    )]
+    fn stack_map(s: &str, expected: bool) {
+        assert_eq!(Solution::stack_map(s.to_string()), expected)
     }
 }
