e2e028126dc90378f1c31e50d71e868cf95d5ec7
Haruyoshi Toyama <duane0728@gmail.com>
2021-01-29 03:08:03 +0900

    Add function

--- rust/src/shared/tree_node.rs
+++ rust/src/shared/tree_node.rs
@@ -11,6 +11,10 @@ pub struct TreeNode {
 }
 
 impl TreeNode {
+    pub fn wrap(self) -> Option<Rc<RefCell<TreeNode>>> {
+        Some(Rc::new(RefCell::new(self)))
+    }
+
     #[inline]
     pub fn new(val: i32) -> Self {
         TreeNode {
@@ -72,7 +76,6 @@ impl TreeNode {
     }
 }
 
-#[derive(Debug)]
 pub enum Value {
     V(i32),
     Null,
@@ -84,7 +87,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn from_array() {
+    fn tree_node_from_array() {
         // https://support.leetcode.com/hc/en-us/articles/360011883654-What-does-1-null-2-3-mean-in-binary-tree-representation-
         assert_eq!(
             TreeNode::from_array(&[V(1)]),
@@ -99,8 +102,8 @@ mod tests {
             TreeNode::from_array(&[V(1), V(2), V(3)]),
             TreeNode {
                 val: 1,
-                left: w(TreeNode::new(2)),
-                right: w(TreeNode::new(3)),
+                left: TreeNode::new(2).wrap(),
+                right: TreeNode::new(3).wrap(),
             }
         );
 
@@ -109,11 +112,12 @@ mod tests {
             TreeNode {
                 val: 1,
                 left: None,
-                right: w(TreeNode {
+                right: TreeNode {
                     val: 2,
-                    left: w(TreeNode::new(3)),
+                    left: TreeNode::new(3).wrap(),
                     right: None
-                }),
+                }
+                .wrap(),
             }
         );
 
@@ -121,24 +125,28 @@ mod tests {
             TreeNode::from_array(&[V(5), V(4), V(7), V(3), Null, V(2), Null, V(-1), Null, V(9)]),
             TreeNode {
                 val: 5,
-                left: w(TreeNode {
+                left: TreeNode {
                     val: 4,
-                    left: w(TreeNode {
+                    left: TreeNode {
                         val: 3,
-                        left: w(TreeNode::new(-1)),
+                        left: TreeNode::new(-1).wrap(),
                         right: None
-                    }),
+                    }
+                    .wrap(),
                     right: None
-                }),
-                right: w(TreeNode {
+                }
+                .wrap(),
+                right: TreeNode {
                     val: 7,
-                    left: w(TreeNode {
+                    left: TreeNode {
                         val: 2,
-                        left: w(TreeNode::new(9)),
+                        left: TreeNode::new(9).wrap(),
                         right: None
-                    }),
+                    }
+                    .wrap(),
                     right: None
-                })
+                }
+                .wrap()
             }
         );
 
@@ -147,27 +155,23 @@ mod tests {
             TreeNode::from_array(&[V(3), V(9), V(20), Null, Null, V(15), V(7)]),
             TreeNode {
                 val: 3,
-                left: w(TreeNode::new(9)),
-                right: w(TreeNode {
+                left: TreeNode::new(9).wrap(),
+                right: TreeNode {
                     val: 20,
-                    left: w(TreeNode::new(15)),
-                    right: w(TreeNode::new(7)),
-                })
+                    left: TreeNode::new(15).wrap(),
+                    right: TreeNode::new(7).wrap(),
+                }
+                .wrap()
             }
         );
     }
 
     #[test]
-    fn from_array_and_wrap() {
+    fn tree_node_from_array_and_wrap() {
         assert_eq!(TreeNode::from_array_and_wrap(&[]), None);
         assert_eq!(
             TreeNode::from_array_and_wrap(&[V(1)]),
-            Some(Rc::new(RefCell::new(TreeNode::new(1))))
+            TreeNode::new(1).wrap()
         )
-    }
-
-    // means wrap
-    fn w(node: TreeNode) -> Option<Rc<RefCell<TreeNode>>> {
-        Some(Rc::new(RefCell::new(node)))
     }
 }
