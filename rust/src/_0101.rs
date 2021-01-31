5ae61c8a20466a6b7a85b9113ef0122c5f5b6a52
Haruyoshi Toyama <duane0728@gmail.com>
2021-01-31 11:08:05 +0900

    Solve with another solution

--- rust/src/_0101.rs
+++ rust/src/_0101.rs
@@ -1,12 +1,14 @@
 use crate::shared::tree_node::TreeNode;
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 struct Solution;
 /// https://leetcode.com/problems/symmetric-tree/
 impl Solution {
     pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        Solution::recursive(root)
+        // Solution::recursive(root)
+        Solution::iterative(root)
     }
 
     /// 0 ms	2.2 MB
@@ -35,6 +37,35 @@ impl Solution {
             true
         }
     }
+
+    /// 0 ms	2.1 MB
+    fn iterative(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        if root.is_none() {
+            return true;
+        }
+
+        let root = root.unwrap().replace(TreeNode::new(1));
+        let mut queue = VecDeque::new();
+        queue.push_front((root.left, root.right));
+
+        while !queue.is_empty() {
+            match queue.pop_back().unwrap() {
+                (Some(l), Some(r)) => {
+                    let l = l.borrow();
+                    let r = r.borrow();
+                    if l.val != r.val {
+                        return false;
+                    }
+                    queue.push_front((l.left.clone(), r.right.clone()));
+                    queue.push_front((l.right.clone(), r.left.clone()));
+                }
+                (None, None) => {}
+                _ => return false,
+            }
+        }
+
+        true
+    }
 }
 
 #[cfg(test)]
@@ -53,6 +84,21 @@ mod tests {
     fn recursive(root: &str, expected: bool) {
         assert_eq!(
             Solution::recursive(TreeNode::from_str_and_wrap(root)),
+            expected
+        )
+    }
+
+    #[rstest(
+        root,
+        expected,
+        case("[]", true),
+        case("[1]", true),
+        case("[1,2,2,3,4,4,3]", true),
+        case("[1,2,2,null,3,null,3]", false)
+    )]
+    fn iterative(root: &str, expected: bool) {
+        assert_eq!(
+            Solution::iterative(TreeNode::from_str_and_wrap(root)),
             expected
         )
     }
