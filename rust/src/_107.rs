98c07207007935cd780abc7a6990db8b5abb90a1
Haruyoshi Toyama <duane0728@gmail.com>
2021-02-11 21:37:50 +0900

    Solve with another solution

--- rust/src/_107.rs
+++ rust/src/_107.rs
@@ -7,11 +7,12 @@ use std::rc::Rc;
 /// https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
 impl Solution {
     pub fn level_order_bottom(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
-        Solution::dfs(root)
+        // Solution::bfs_iterative(root)
+        Solution::dfs_recursive(root)
     }
 
     /// 0 ms	2.2 MB
-    fn dfs(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+    fn bfs_iterative(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
         if root.is_none() {
             return vec![];
         }
@@ -45,6 +46,32 @@ impl Solution {
         result.reverse();
         result
     }
+
+    /// 0 ms	2.3 MB
+    fn dfs_recursive(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        fn helper(node: Option<Rc<RefCell<TreeNode>>>, level: usize, result: &mut Vec<Vec<i32>>) {
+            if node.is_none() {
+                return;
+            }
+
+            let node = (*node.unwrap()).replace(TreeNode::new(0));
+            if let Some(vec) = result.get_mut(level - 1) {
+                vec.push(node.val)
+            } else {
+                let vec = vec![node.val];
+                result.push(vec);
+            }
+
+            let next = level + 1;
+            helper(node.left, next, result);
+            helper(node.right, next, result);
+        }
+
+        let mut result = vec![];
+        helper(root, 1, &mut result);
+        result.reverse();
+        result
+    }
 }
 
 #[cfg(test)]
@@ -54,14 +81,41 @@ mod tests {
 
     #[rstest(root, expected,
     case(
-    "[3,9,20,null,null,15,7]",
-    vec![
-    vec![15,7],
-    vec![9,20],
-    vec![3]
-    ]),
+        "[]",
+        vec![]
+    ),
+    case(
+        "[3,9,20,null,null,15,7]",
+         vec![
+            vec![15,7],
+            vec![9,20],
+            vec![3]
+        ]),
     )]
-    fn dfs(root: &str, expected: Vec<Vec<i32>>) {
-        assert_eq!(Solution::dfs(TreeNode::from_str_and_wrap(root)), expected);
+    fn bfs_iterative(root: &str, expected: Vec<Vec<i32>>) {
+        assert_eq!(
+            Solution::bfs_iterative(TreeNode::from_str_and_wrap(root)),
+            expected
+        );
+    }
+
+    #[rstest(root, expected,
+    case(
+        "[]",
+        vec![]
+    ),
+    case(
+        "[3,9,20,null,null,15,7]", 
+        vec![
+            vec![15,7],
+            vec![9,20],
+            vec![3]
+        ]),
+    )]
+    fn dfs_recursive(root: &str, expected: Vec<Vec<i32>>) {
+        assert_eq!(
+            Solution::dfs_recursive(TreeNode::from_str_and_wrap(root)),
+            expected
+        );
     }
 }
