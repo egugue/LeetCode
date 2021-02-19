dc2fac517253145b422d7ad689fbf9d5603d4401
Haruyoshi Toyama <duane0728@gmail.com>
2021-02-19 23:00:30 +0900

    Solve with another solution

--- rust/src/_0111.rs
+++ rust/src/_0111.rs
@@ -2,7 +2,6 @@ struct Solution;
 
 use crate::shared::tree_node::TreeNode;
 use std::cell::RefCell;
-use std::cmp::min;
 use std::rc::Rc;
 
 /// 111. Minimum Depth of Binary Tree
@@ -13,6 +12,8 @@ impl Solution {
 
     /// 36 ms	13.7 MB
     fn recursive(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        use std::cmp::min;
+
         fn min_depth(node: TreeNode, cur_depth: i32) -> i32 {
             if node.left.is_none() && node.right.is_none() {
                 return cur_depth;
@@ -33,6 +34,41 @@ impl Solution {
 
         if let Some(root) = root {
             min_depth(root.replace(TreeNode::new(0)), 1)
+        } else {
+            0
+        }
+    }
+
+    /// 40 ms	13.1 MB
+    fn recursive_ref(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        use std::cmp::min;
+        use std::ops::Deref;
+
+        fn min_depth(node: &TreeNode, cur_depth: i32) -> i32 {
+            if node.left.is_none() && node.right.is_none() {
+                return cur_depth;
+            }
+
+            let left_depth = if let Some(node) = node.left.as_ref() {
+                let node_ref = node.deref().borrow();
+                min_depth(node_ref.deref(), cur_depth + 1)
+            } else {
+                std::i32::MAX
+            };
+
+            let right_depth = if let Some(node) = node.right.as_ref() {
+                let node_ref = node.deref().borrow();
+                min_depth(node_ref.deref(), cur_depth + 1)
+            } else {
+                std::i32::MAX
+            };
+
+            min(left_depth, right_depth)
+        }
+
+        if let Some(node) = root.as_ref() {
+            let node_ref = node.deref().borrow();
+            min_depth(node_ref.deref(), 1)
         } else {
             0
         }
