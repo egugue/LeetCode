20c0e88dbd939239f22d0c44c9faf90f650151bc
Haruyoshi Toyama <duane0728@gmail.com>
2021-04-11 23:39:10 +0900

    Solve 217. Contains Duplicate

---
+++ rust/src/_0217.rs
@@ -0,0 +1,32 @@
+struct Solution;
+
+/// https://leetcode.com/problems/contains-duplicate/
+impl Solution {
+    /// 0 ms	2.7 MB
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        if nums.len() <= 1 {
+            return false;
+        }
+
+        let mut nums = nums;
+        nums.sort();
+
+        let mut prev = nums[0];
+        for &num in &nums[1..] {
+            if prev == num {
+                return true;
+            }
+            prev = num;
+        }
+
+        false
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn test() {
+
+    }
