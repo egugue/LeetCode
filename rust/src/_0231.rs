e910c532f13294db9cd3e3eb1b290b5b55094442
Haruyoshi Toyama <duane0728@gmail.com>
2021-04-30 23:39:15 +0900

    Solve with another solution

--- rust/src/_0231.rs
+++ rust/src/_0231.rs
@@ -3,15 +3,35 @@ struct Solution;
 /// https://leetcode.com/problems/power-of-two/
 impl Solution {
     pub fn is_power_of_two(n: i32) -> bool {
-        Solution::bit(n)
+        Solution::iterative(n)
     }
 
     /// 0 ms	2 MB
     fn bit(n: i32) -> bool {
+        // should use <= because (n & (i32::min_value() - 1)) = true
         if n <= 0 {
             return false;
         }
         (n & (n - 1)) == 0
+    }
+
+    /// 0 ms	2 MB
+    fn iterative(n: i32) -> bool {
+        if n <= 0 || n % 2 == 1 {
+            return false;
+        }
+        if n == 1 {
+            return true;
+        }
+
+        let mut n = n;
+        while n != 2 {
+            if n & 1 == 1 {
+                return false;
+            }
+            n >>= 1;
+        }
+        true
     }
 }
 
